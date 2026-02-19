5af50235ae07f106e95cc1b22dc92bcc9c78e8fc
SanderSaska <sander.saska@gmail.com>
2026-02-19 12:10:24 +0200

    Homonym dataset scripts

---
+++ scripts/preprocessing/homonym/extract.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import typing
+
+from estnltk.converters.label_studio.labelling_configurations import (
+    PhraseClassificationConfiguration,
+)
+from estnltk.converters.label_studio.labelling_tasks import PhraseClassificationTask
+
+
+class Extractor:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def extract(input_dir: typing.Union[str, Path]):
+
+        # Collect input files
+        input_files = []
+        input_dir = Path(input_dir)
+        for fname in os.listdir(input_dir):
+            if os.path.isdir(os.path.join(input_dir, fname)):
+                for subfname in os.listdir(os.path.join(input_dir, fname)):
+                    if subfname.endswith(".json"):
+                        inflection_type = int(
+                            subfname.split("_")[2]
+                        )  # infl_type_xx_1000_vx...
+                        input_files.append(
+                            (
+                                inflection_type,
+                                str(input_dir) + "/" + fname + "/" + subfname,
+                            )
+                        )
+
+        if not input_files:
+            raise RuntimeError("No input files found!")
+
+        print(f"Found {len(input_files)} input files.")
+
+        return input_files
---
+++ scripts/preprocessing/homonym/preprocess.py
@@ -0,0 +1,94 @@
+import typing
+import pandas as pd
+from pathlib import Path
+
+from estnltk.converters.label_studio.labelling_configurations import (
+    PhraseClassificationConfiguration,
+)
+from estnltk.converters.label_studio.labelling_tasks import PhraseClassificationTask
+
+
+class Extractor:
+    def __init__(self):
+        # Use specific annotation configurations that were used in homonyms dataset
+        self.annotation_confs = {
+            1: PhraseClassificationConfiguration(
+                phrase_labels=["analüüsitav sõna"],
+                class_labels={"sg n": "sg n", "sg g": "sg g"},
+                header="Vali sõna morfoloogiline vorm (sg n - ainsuse nimetav, sg g -- ainsuse omastav):",
+                header_placement="middle",
+            ),
+            16: PhraseClassificationConfiguration(
+                phrase_labels=["analüüsitav sõna"],
+                class_labels={"sg n": "sg n", "sg g": "sg g"},
+                header="Vali sõna morfoloogiline vorm (sg n - ainsuse nimetav, sg g -- ainsuse omastav):",
+                header_placement="middle",
+            ),
+            17: PhraseClassificationConfiguration(
+                phrase_labels=["analüüsitav sõna"],
+                class_labels={"sg n": "sg n", "sg g": "sg g", "sg p": "sg p"},
+                header="Vali sõna morfoloogiline vorm (sg n - ainsuse nimetav, sg g - ainsuse omastav, sg p - ainsuse osastav):",
+                header_placement="middle",
+            ),
+            19: PhraseClassificationConfiguration(
+                phrase_labels=["analüüsitav sõna"],
+                class_labels={"sg g": "sg g", "sg p": "sg p", "adt": "adt"},
+                header="Vali sõna morfoloogiline vorm (sg g - ainsuse omastav, sg p - ainsuse osastav, adt - lühike sisseütlev):",
+                header_placement="middle",
+            ),
+        }
+        return self.annotation_confs
+
+    def create_df(
+        self,
+        input_files: typing.List[typing.Tuple],
+        output_dir: typing.Union[str, Path],
+        do_individual_dfs: bool = True,
+        do_overall_df: bool = True,
+    ):
+
+        overall_data = []
+        # Extract data from input files
+        for infl_type, input_file in input_files:
+            print(f"Processing file: {input_file} (inflection type {infl_type})")
+            num = input_file.split("/")[3]
+            annotation_conf = self.annotation_confs[infl_type]
+            with open(input_file, "r", encoding="utf-8") as f:
+                raw = f.read()
+            task = PhraseClassificationTask(
+                annotation_conf,
+                input_layer="morph",
+                output_layer="morph",
+                label_attribute="label",
+            )
+            classified_sentences = task.import_data(raw)
+
+            # Create dataframe from classified sentences
+            data = []
+            for sentence in classified_sentences:
+                for annotation in sentence.morph:
+                    if (
+                        "class_label" in sentence.meta
+                        and sentence.meta["class_label"] is not None
+                    ):
+                        data.append(
+                            {
+                                "num": num,
+                                "inflection_type": infl_type,
+                                "sentence": sentence.text,
+                                "word": annotation.text,
+                                "word_span": (annotation.start, annotation.end),
+                                "label": sentence.meta["class_label"],
+                            }
+                        )
+            df = pd.DataFrame(data)
+            output_csv = f"{output_dir}/homonyms_infltype_{num}_{infl_type}.csv"
+            df.to_csv(output_csv, index=False)
+            print(f"Saved processed data to {output_csv}")
+            overall_data.extend(data)
+
+        # Create overall dataframe
+        overall_df = pd.DataFrame(overall_data)
+        overall_output_csv = f"{output_dir}/homonyms_overall.csv"
+        overall_df.to_csv(overall_output_csv, index=False)
+        print(f"Saved overall processed data to {overall_output_csv}")
